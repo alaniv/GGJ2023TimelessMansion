6f24ab8741de70b342ef194bb1ec3b36a7e26a1c
alaniv <ivoalani@outlook.com>
2023-02-04 09:44:16 -0300

    resolviendo un todo en dialog

--- Prototype/Dialog/DialogManager/DialogManager.gd
+++ Prototype/Dialog/DialogManager/DialogManager.gd
@@ -1,22 +1,24 @@
 extends Control
 signal dialog_ended
 
-# todo: put everything in another child, to avoid having to set visible false in Main Node
+#todo: add a DialogBottom TextBox, if needed
+func _ready():
+	$DialogTop.visible = false
 
 func show_message(text : String):
-	visible = true
-	$RichTextLabel.text = text
+	$DialogTop.visible = true
+	$DialogTop/RichTextLabel.text = text
 	PrototypeGlobals.set_deferred("pause", true)
 
 func show_message_not_blocking(text : String):
-	visible = true
-	$RichTextLabel.text = text
+	$DialogTop.visible = true
+	$DialogTop/RichTextLabel.text = text
 	$Timer.start(1)
 	yield($Timer, "timeout")
-	visible = false
+	$DialogTop.visible = false
 	
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_accept") && visible:
-		visible = false
+	if Input.is_action_just_pressed("ui_accept") && $DialogTop.visible:
+		$DialogTop.visible = false
 		PrototypeGlobals.set_deferred("pause", false)
 		emit_signal("dialog_ended")
